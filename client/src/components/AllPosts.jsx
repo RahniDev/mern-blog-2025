574868bce473125f4e9f6b1d570a04340f96d562
RahniDev <rahni32@protonmail.com>
2025-06-10 20:58:03 +0200

    Get all posts and render them

--- client/src/components/AllPosts.jsx
+++ client/src/components/AllPosts.jsx
@@ -1,7 +1,41 @@
+import { useState, useEffect } from 'react'
 
 const AllPosts = () => {
+  const [allPosts, setAllPosts] = useState([])
+  const [postsList, setPostsList] = useState()
+
+  const getAllPosts = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/posts/', {
+        method: 'GET'
+      });
+      const data = await response.json();
+      setAllPosts(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const getPosts = () => {
+    for (let i = 0; i < allPosts.length; i++) {
+      let posts = allPosts[i];
+      setPostsList(posts);
+    }
+  }
+
+  useEffect(() => {
+    getAllPosts()
+  }, [])
+
+  useEffect(() => {
+    getPosts()
+  }, [allPosts])
+
   return (
-    <div>AllPosts</div>
+    <>
+      {postsList &&
+        <div><h2>{postsList.title}</h2><p>{postsList.body}</p></div>
+      }
+    </>
   )
 }
 
